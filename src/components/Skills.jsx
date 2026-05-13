3da7403041384082232e88516ee3ccdfe1edd13c
Unaib31 <unaibshaikh632gmail.com>
2026-05-14 01:08:24 +0530

    Add skills section with dynamic card

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,7 +1,38 @@
-export default function Skills() {
-  return (
-    <section>
-      <h2>Skills Section</h2>
-    </section>
-  );
+function Skills() {
+
+    const skills = [
+        "HTML",
+        "CSS",
+        "JavaScript",
+        "React",
+        "Java",
+        "Python",
+        "Git",
+        "GitHub",
+        "SQL"
+    ]
+
+    return (
+        <section id="skills" className="skills">
+
+            <h1 className="section-title">
+                Skills
+            </h1>
+
+            <div className="skills-container">
+
+                {
+                    skills.map((skill, index) => (
+                        <div className="skill-card" key={index}>
+                            {skill}
+                        </div>
+                    ))
+                }
+
+            </div>
+
+        </section>
+    )
 }
+
+export default Skills
