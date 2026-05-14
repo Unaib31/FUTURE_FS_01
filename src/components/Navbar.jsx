4f9c1e7638bac3de7f6137e4e2c78c8de1a31431
Unaib31 <unaibshaikh632gmail.com>
2026-05-14 12:56:33 +0530

    Makes header Clickable

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,11 +4,11 @@ function Navbar() {
             <h1 className="logo">Unaib</h1>
 
             <ul className="nav-links">
-                <li>Home</li>
-                <li>About</li>
-                <li>Skills</li>
-                <li>Projects</li>
-                <li>Contact</li>
+                <li><a href="#home">Home</a></li>
+                <li><a href="#about">About</a></li>
+                <li><a href="#skills">Skills</a></li>
+                <li><a href="#projects">Projects</a></li>
+                <li><a href="#contact">Contact</a></li>
             </ul>
         </nav>
     )
